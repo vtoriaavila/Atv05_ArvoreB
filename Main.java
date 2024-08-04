887f72f4e071d4ff1436781fc7930757830ba00b
Chebl <jtchebl@outlook.com>
2024-08-04 14:55:48 -0300

    melhorando o menu, e separando a a impressao das medias

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
@@ -13,70 +17,78 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         arvoreB<Elemento> bTree = new arvoreB<>(3); // Cria uma árvore B de ordem 3
-        String arquivoCsv =  "src/dados/anac_174932488/202402.CSV";
-        List<Elemento> elementos = readCsv.lerCsv(arquivoCsv);
-//        int[] values = {45, -10, 67, 2, 33, 99};
-//        for (int value : values) {
-//            bTree.inserir(value);
+        List<String> arquivosCsv = new ArrayList<>();
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get("src/dados/anac_174932488"), "*.CSV")) {
+            for (Path entry : stream) {
+                arquivosCsv.add(entry.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        arquivosCsv.add("src/dados/anac_174932488/202406.CSV");
+
+        // Ler e inserir elementos de cada arquivo CSV
+//        for (String arquivoCsv : arquivosCsv) {
+//            List<Elemento> elementos = readCsv.lerCsv(arquivoCsv);
+//            for (Elemento elemento : elementos) {
+//                bTree.inserir(elemento); // Insere o elemento na árvore
+//            }
 //        }
 
         while (true) {
             System.out.println("\nMenu:");
             System.out.println("1. Inserir um valor");
-            System.out.println("2. Buscar um valor");
-            System.out.println("3. Remover um valor");
-            System.out.println("4. Imprimir a árvore");
-            System.out.println("5. Sair");
+            System.out.println("2. Calcular média");
+            System.out.println("3. Imprimir a árvore");
+            System.out.println("4. Sair");
             System.out.print("Escolha uma opção: ");
 
             int choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-//                    System.out.print("Digite o valor a ser inserido: ");
-//                    Elemento insertValue = scanner.nextInt();
-//                    bTree.inserir(insertValue);
-                    for (Elemento elemento : elementos) {
-                        bTree.inserir(elemento); // Insere o elemento na árvore
+                    for (String arquivoCsv : arquivosCsv) {
+                        List<Elemento> elementos = readCsv.lerCsv(arquivoCsv);
+                        for (Elemento elemento : elementos) {
+                            bTree.inserir(elemento); // Insere o elemento na árvore
+                        }
                     }
-                    System.out.println("arvore inserida");
-//                    System.out.println("Valor inserido com sucesso.");
+                    System.out.println("Valores inseridos com sucesso.");
                     break;
 
                 case 2:
-//                    System.out.print("Digite o valor a ser buscado: ");
-//                    int searchValue = scanner.nextInt();
-//                   // Elemento elementoBuscado = new Elemento(searchValue);
-//                    boolean found = bTree.buscar(searchValue);
-//                    if (found) {
-//                        System.out.println("Valor encontrado na árvore.");
-//                    } else {
-//                        System.out.println("Valor não encontrado na árvore.");
-//                    }
+                    System.out.println("Calculo Media");
+                    System.out.println("1- Media geral");
+                    System.out.println("2- Media por empresa");
+                    System.out.println("3- Media por tempo");
+                    int escolha = scanner.nextInt();
+                    switch (escolha) {
+                        case 1:
+                            bTree.printarMediaG();
+                            break;
+
+                        case 2:
+                            bTree.printarMediaEmp();
+                            break;
+
+                        case 3:
+                            bTree.printarMediaTemp();
+                            break;
+
+
+                    };
+
                     break;
 
                 case 3:
-//                    System.out.print("Digite o valor a ser removido: ");
-//                    int deleteValue = scanner.nextInt();
-//                    boolean pass = bTree.buscar(deleteValue);
-//                    if (pass) {
-//                        bTree.remover(deleteValue);
-//                        System.out.println("Valor deletado da árvore.");
-//                    } else {
-//                        System.out.println("Valor não encontrado na árvore, não é possível remover");
-//                    }
-                    break;
-
-                case 4:
                     System.out.println("Imprimindo a árvore:");
                     bTree.printarArvore();
 //                    bTree.imprimirParaArquivo("src/dados/arquivoSaida.txt");
 
                     //ler.lerCsv(arquivoCsv);
-
                     break;
-
-                case 5:
+                case 4:
                     System.out.println("Saindo...");
                     scanner.close();
                     return;
